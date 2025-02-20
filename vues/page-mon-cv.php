12702646ec8d499a648c00a61f8b946b530279f7
fabienmacip <77588015+fabienmacip@users.noreply.github.com>
2025-02-20 10:50:06 +0100

    FIX : Page 'mon CV' display ok

--- vues/page-mon-cv.php
+++ vues/page-mon-cv.php
@@ -1,6 +1,6 @@
 <div id="mon-cv-div">
     <h1>D&eacute;veloppeur web Fullstack</h1>
-    <a  href="https://www.fatabien.com/webfm.fr/assets/doc/CV-Developpeur-web-fullstack---Fabien-MACIP.pdf" 
+    <a  href="assets/doc/CV_Developpeur_web_fullstack_TS_JS___Fabien_MACIP.pdf" 
         target="_blank"
         class="link-to-pdf"
     >
@@ -10,18 +10,10 @@
         <img src="img/mon-cv/fabien-macip.jpg" alt="fabien macip photo identité" class="box">
     </div>
 
-    <div id="mon-cv-imgs">
-        <div class="pdf-container">
-            <iframe 
-                src="https://www.fatabien.com/webfm.fr/assets/doc/CV-Developpeur-web-fullstack---Fabien-MACIP.pdf"
-                width="100%" 
-                height="800px" 
-                style="border: none;"
-                title="CV Fabien MACIP">
-                <p>Votre navigateur ne supporte pas l'affichage des PDF. 
-                    <a href="https://www.fatabien.com/webfm.fr/assets/doc/CV-Developpeur-web-fullstack---Fabien-MACIP.pdf">Cliquez ici pour télécharger le PDF</a>
-                </p>
-            </iframe>
-        </div>
+    <div id="mon-cv-imgs">  
+        <img 
+            src="assets/img/CV_Developpeur_web_fullstack_TS_JS___Fabien_MACIP.jpg" 
+            alt="fabien macip cv en .jpg"
+        >
     </div>
 </div>
